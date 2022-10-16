0af4ffd3ecbc440eef46474ecd60d89b3d9b4d9a
Sergey <barmaley78@mail.ru>
2022-10-16 13:10:46 +0700

    решение 3 задачи

---
+++ hw1/ex3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+string numStr = Console.ReadLine();
+int num = int.Parse(numStr);
+if(num % 2 == 0)
+{
+    Console.WriteLine($"Число {num} четное");
+}
+else
+{
+    Console.WriteLine($"Число {num} нечетное");
+}
