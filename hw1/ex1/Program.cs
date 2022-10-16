eb1d664608833b279608a178ae7717016b38ec75
Sergey <barmaley78@mail.ru>
2022-10-16 12:55:19 +0700

    решение 1 задачи

--- hw1/ex1/Program.cs
+++ hw1/ex1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите два числа");
+string num1Str = Console.ReadLine();
+string num2Str = Console.ReadLine();
+int num1 = int.Parse(num1Str);
+int num2 = int.Parse(num2Str);
+if(num1 > num2)
+{
+    Console.WriteLine($"max = {num1} min = {num2}");
+}
+else
+if(num1 < num2)
+{
+    Console.WriteLine($"max = {num2} min = {num1}");
+}
+else
+{
+    Console.WriteLine($"{num1} = {num2}");
+}
+
