c6198e166c476ab0dea9f47a31b8edbea81610c6
Sergey <barmaley78@mail.ru>
2022-10-16 15:08:07 +0700

    сделал 2-ю домашку

---
+++ hw2/ex3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите номер дня ");
+string dayNumStr = Console.ReadLine();
+int dayNum = Math.Abs(int.Parse(dayNumStr));
+if(dayNum == 0 || dayNum == 6)
+    Console.WriteLine("Выходной");
+else
+    Console.WriteLine("Не выходной");
