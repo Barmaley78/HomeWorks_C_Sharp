3b611751c6bb23d2fa61b5d72b3eca9779c731df
Sergey <barmaley78@mail.ru>
2022-10-16 13:19:40 +0700

    решение 4 задачи

---
+++ hw1/ex4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число ");
+string numStr = Console.ReadLine();
+int num = int.Parse(numStr);
+int count = 2;
+while(count <= num - ( num % 2 ))
+{
+Console.Write($"{count} ");
+count = count + 2;
+}
