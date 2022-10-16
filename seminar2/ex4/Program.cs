6f7f906d91a4f72aa41abdbb9bf8776a4177bf74
Sergey <barmaley78@mail.ru>
2022-10-16 15:27:32 +0700

    закончили семинар 2

--- seminar2/ex4/Program.cs
+++ seminar2/ex4/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+int GetNumber(string mess)
+{
+    Console.WriteLine(mess);
+    string numStr = Console.ReadLine();
+    int num = int.Parse(numStr);
+    return num;
+}
+
+int num1 = GetNumber("Введите число ");
+if(num1 % 23 == 0 && num1 % 7 == 0)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
---
+++ seminar2/ex5/Program.cs
@@ -0,0 +1,28 @@
+﻿// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+int GetNumber(string mess)
+{
+    Console.WriteLine(mess);
+    string numStr = Console.ReadLine();
+    int num = int.Parse(numStr);
+    return num;
+}
+
+int num1 = GetNumber("Введите 1-е число ");
+int num2 = GetNumber("Введите 2-е число ");
+if(num2 < num1)
+{
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+if(num1 * num1 == num2)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
