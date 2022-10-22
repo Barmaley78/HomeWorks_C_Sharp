1e49e43ccaf72dc085549e6cea1c0aae553755d9
Sergey <barmaley78@mail.ru>
2022-10-22 14:39:18 +0700

    решение 1 задачи

--- seminar3/ex1/Program.cs
+++ seminar3/ex1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int GetNumber(string mess)
+{
+    int result;
+    while (true)
+    {
+    Console.WriteLine(mess);
+    if(int.TryParse(Console.ReadLine(), out int num) && num != 0)
+    {
+        result = num;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Ввели не число или число равное нулю. повторите ввод \n");
+    }
+    }   
+    return result;
+}
+
+int GetXY(int x, int y)
+{
+    if(x > 0 && y > 0) return 1;
+    else 
+    if(x < 0 && y > 0) return 2;
+    else 
+    if(x < 0 && y < 0) return 3; 
+    else return 4;
+}
+int numX = GetNumber("Введите X ");
+int numY = GetNumber("Введите Y ");
+int Q = GetXY(numX, numY);
+Console.WriteLine($"точка с координатами ({numX} {numY} находится в {Q} четверти)");
---
+++ seminar3/ex2/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+int GetNum(string mess)
+{
+    int result;
+while (true)
+{
+    Console.WriteLine(mess);
+    if(int.TryParse(Console.ReadLine(), out int num) && num != 0)
+    {
+        result = num;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Ввели не число или число равное нулю. повторите ввод \n");
+    }
+}
+    return result;
+}
+
+void GQ(int x)
+{
+    switch (x)
+    {
+        case 1:
+            Console.WriteLine("x>0; y>0");
+            break;
+        case 2:
+            Console.WriteLine("x<0; y>0");
+            break;
+        case 3:
+            Console.WriteLine("x<0; y<0");
+            break;
+        case 4:
+            Console.WriteLine("x>0; y<0");
+            break;
+        default:
+            Console.WriteLine("fffffff");
+            break;
+    }
+}
+
+int x = GetNum("Введите  номер четверти");
+GQ(x);
