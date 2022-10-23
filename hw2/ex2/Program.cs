7505a0b39db007902a303e9d50c637c354ae2dbf
Sergey <barmaley78@mail.ru>
2022-10-23 16:27:00 +0700

    исправил 2 задачу дз2 в соответсвии с примером

--- hw2/ex2/Program.cs
+++ hw2/ex2/Program.cs
@@ -1,8 +1,20 @@
-﻿Console.WriteLine("Введите число ");
+﻿// Console.WriteLine("Введите число ");
+// string numStr = Console.ReadLine();
+// int num = Math.Abs(int.Parse(numStr));
+// int res = num / 100;
+// if(res > 0)
+//     Console.WriteLine($"Третья цифра числа {res % 10}");
+// else
+//     Console.WriteLine($"Третья цифра в числе отсутсвует");
+
+Console.WriteLine("Введите число ");
 string numStr = Console.ReadLine();
 int num = Math.Abs(int.Parse(numStr));
-int res = num / 100;
-if(res > 0)
-    Console.WriteLine($"Третья цифра числа {res % 10}");
-else
-    Console.WriteLine($"Третья цифра в числе отсутсвует");
+int[] arr = new int[numStr.Length];
+for(int i = numStr.Length - 1; i >= 0 ; i--)
+{
+    arr[i] = num % 10;
+    num = num / 10;
+}
+if(numStr.Length > 3) Console.WriteLine($"третья цифра {arr[2]}");
+else Console.WriteLine($"нет третьей цифры");
