e9f737d0bddcc91b369c524f5b0d8c6213390b38
Sergey <barmaley78@mail.ru>
2022-11-26 15:28:21 +0700

    закончил домашку 9

---
+++ seminar9/ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные 
+числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6" */
+
+
+int GetNumber(string msg)
+{
+    Console.Write(msg);
+    int userNumber = Convert.ToInt32(Console.ReadLine());;
+    return userNumber;
+}
+
+string perebor(int num)
+{
+    string str = string.Empty;
+    if (num == 1) return num.ToString();
+    return perebor(num - 1) + " " + num;
+}
+
+int m = GetNumber("Введите число строк: ");
+Console.WriteLine();
+Console.WriteLine(perebor(m));
---
+++ seminar9/ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А 
+в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8 */
+
+int GetNumber(string msg)
+{
+    Console.WriteLine(msg);
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    return userNumber;
+}
+
+int DegreeOfDigit(int numA, int numB)
+{
+    if(numB == 1) return numA;
+    else
+    {
+        return numA * DegreeOfDigit(numA, numB - 1);
+    }
+}    
+
+int userNumberA = GetNumber("Введите число A: ");
+int userNumberB = GetNumber("Введите число B: ");
+int res = DegreeOfDigit(userNumberA, userNumberB);
+Console.WriteLine(res);
